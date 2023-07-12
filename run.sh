b5ccf39aaad536e1876941f2f4a6f39a3d371c85
Salahuddin <Salahuddinsaiet.10@gmail.com>
2023-07-12 12:22:37 +0200

    Updated aliases and run.sh.

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
-# gsettings
-gsettings set org.gnome.shell.app-switcher current-workspace-only true #Only alt+tab in current workspace
-gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.4 #Dock opacity
+settings() {
+	gsettings set org.gnome.shell.app-switcher current-workspace-only true #Only alt+tab in current workspace
+	gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.4 #Dock opacity
+}
 
-# Variables
-CONFIG_PATH="$HOME/.config"
+update() {
+	CONFIG_PATH="$HOME/.config"
+	mkdir -p $CONFIG_PATH
+	cp -v .zshenv $HOME
+	cp -rv ./zsh $CONFIG_PATH
+	cp -rv nvim $CONFIG_PATH
+}
 
-
-# Move files
-mkdir -p $CONFIG_PATH
-cp -v .zshenv $HOME
-cp -rv ./zsh $CONFIG_PATH
+while getopts 'su' OPTION; do
+	case "$OPTION" in
+		s)
+			settings
+			;;
+		u)
+			update
+			;;
+		su)
+			settings
+			update
+			;;
+	esac
+done
